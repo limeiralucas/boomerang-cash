6ad3f487b90cf2254d0f59f07ff0487dbd5c76e9
Lucas Limeira <lucasalveslm@gmail.com>
2024-12-17 21:58:24 -0300

    feat: added settings module

---
+++ app/settings.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    """
+    Settings configuration for the Boomerang Cash application.
+
+    Attributes:
+        APP_NAME (str): The name of the application. Default is "Boomerang Cash".
+        MONGO_CONN_STR (str): The connection string for the MongoDB database.
+    """
+
+    APP_NAME: str = "Boomerang Cash"
+    MONGO_CONN_STR: str
+
+    model_config = SettingsConfigDict(env_file=".env")
