7e973aa18117685c123a030263171400f93fab9d
Lucas Limeira <lucasalveslm@gmail.com>
2024-12-18 12:50:40 -0300

    feat: mocking order repository functions

--- app/adapters/repositories/order.py
+++ app/adapters/repositories/order.py
@@ -1,18 +1,23 @@
 from typing import override
-from beanie import Document
 from app.domain.models.order import Order
 from app.domain.ports.order import OrderPort
-
-
-class OrderDocument(Order, Document):
-    pass
 
 
 class OrderRepository(OrderPort):
     @override
     async def create_order(self, order: Order) -> Order:
-        return await OrderDocument.insert(order)
+        return Order(
+            code="123",
+            value=100,
+            reseller_cpf="06289049089",
+        )
 
     @override
     async def list_orders(self) -> list[Order]:
-        return await OrderDocument.all()
+        return [
+            Order(
+                code="123",
+                value=100,
+                reseller_cpf="06289049089",
+            )
+        ]
